5e4f306becc92b5d7f00ecd9a12d9d43852ecef9
Ben Poon <benjamintpoon@gmail.com>
2022-11-05 21:24:48 -0500

    Hello World with StimulusJS

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['list']
+
   connect() {
-    this.element.textContent = "Hello World!"
+  	console.log('connect()')
+  }
+  addHelloWorld() {
+	const listEl = this.listTarget
+	let itemEl = document.createElement('li')
+	itemEl.innerHTML = "Hello World from Stimulus controller"
+	listEl.appendChild(itemEl)
   }
 }
+
+// export default class extends Controller {
+//   static targets = [ "list" ]
+//
+//   addHelloWorld() {
+//     const listEl = this.listTarget
+//     var itemEl = document.createElement("li");
+//     itemEl.innerHTML = "Hello World from a Stimulus controller!";
+//     listEl.appendChild(itemEl);
+//   }
+// }
